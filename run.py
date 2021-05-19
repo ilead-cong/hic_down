211bbb0abc8d3cc948be9d098b3c18a7c36985fe
ilead <58165829+ilead-cong@users.noreply.github.com>
2021-05-19 18:47:40 +0800

    compartment analysis need some fixing

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from config_wr.config_write import cfg_wt
 from bin.tad_analysis import tad
 from bin.loop_analysis import loop
 from bin.contact_chrom import contact
-from bin.compartment import compartment
+#from bin.compartment import compartment
 
 cfg_path = os.getcwd()
 
@@ -26,8 +26,8 @@ if len(sys.argv) == 2:
         tad(f"{cfg_path}/config_hic-down.ini")
     elif ("-l" in sys.argv) or ("--loop" in sys.argv):
         loop(f"{cfg_path}/config_hic-down.ini")
-    elif ("-b" in sys.argv) or ("--compartment" in sys.argv):
-        compartment(f"{cfg_path}/config_hic-down.ini")
+    #elif ("-b" in sys.argv) or ("--compartment" in sys.argv):
+        #compartment(f"{cfg_path}/config_hic-down.ini")
     elif ("-a" in sys.argv) or ("--all" in sys.argv):
         tad(f"{cfg_path}/config_hic-down.ini")
         loop(f"{cfg_path}/config_hic-down.ini")
